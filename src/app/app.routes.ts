425bface0024ce6cf5abe30ec6708788061d6252
Esteban Maya <juanestebanmayasanchez@gmail.com>
2026-03-07 14:27:43 -0500

    Upgraded inventory view and supplier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/pages/login/login';
 import { InventoryComponent } from './features/inventory/Inventory';
+import { SuppliersComponent } from './features/suppliers/Suppliers';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'inventory', component: InventoryComponent },
   { path: 'inventory/dashboard', component: InventoryComponent },
-  { path: 'inventory/providers', component: InventoryComponent },
-  { path: 'inventory/providers/new', component: InventoryComponent },
+  { path: 'inventory/providers', component: SuppliersComponent },
   { path: 'inventory/history/supply', component: InventoryComponent },
   { path: 'inventory/history/movements', component: InventoryComponent }
 ];
